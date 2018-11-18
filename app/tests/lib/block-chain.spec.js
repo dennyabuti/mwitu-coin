a8235d68f40f4b42ad939b95eec71cc4010fdda1
Dennis.Nyabuti <dennis@nyabuti.com>
2018-11-18 17:59:05 -0600

    unit test add nodes

--- app/tests/lib/block-chain.spec.js
+++ app/tests/lib/block-chain.spec.js
@@ -1,34 +1,41 @@
 const BlockChain = require('../../lib/block-chain');
-const crypto = require('crypto');
 
 describe('BlockChain Class', () => {
-    let blockChain;
-    beforeEach(() => {
-        blockChain = new BlockChain();
+  let blockChain;
+  beforeEach(() => {
+    blockChain = new BlockChain();
+  });
+  it('Should have a genesis block', () => {
+    expect(blockChain.chain.length).toBe(1);
+  });
+  it('Should register a list of nodes', () => {
+    const nodes = ['localhost:4040', 'localhost:4041', 'localhost:4042'];
+    nodes.forEach(node => {
+      blockChain.registerNode(node);
     });
-    it('Should have a genesis block', () => {
-        expect(blockChain.chain.length).toBe(1);
-    });
-    it('Should register a list of nodes', () => {
-        expect(1 + 1).toBe(2); // Todo
-    });
-    // it('Should check if proof is valid', () =>{
-    //     expect(1 + 1).toBe(3); // Todo
-    // });
-    // it('Should mine a new block', () => {
-    //     expect(1 + 1).toBe(3); // Todo
-    // });
-    // it('Should resolve conflicts', () => {
-    //     expect(1 + 1).toBe(3); // Todo
-    // });
-    // it('Should show proof of work', () => {
-    //     expect(1 + 1).toBe(3); // Todo
-    // });
-    // it('Should check for a valid chain', () => {
-    //     expect(1 + 1).toBe(3); // Todo
-    // });
-    // it('Should check the validity of a chain', () => {
-    //     expect(1 + 1).toBe(3); // Todo
-    // });
+    expect(Array.from(blockChain.nodes).length).toBe(3);
+  });
+  // it.only('Should create a new hash string from json Object', () => {
+  //   const mock = jest.fn();
+  //   process.env.HASH_TYPE = '0000'
+  //   expect(1 + 1).toBe(2)
+  // });
+  // it('Should check if proof is valid', () =>{
+  //     expect(1 + 1).toBe(3); // Todo
+  // });
+  // it('Should mine a new block', () => {
+  //     expect(1 + 1).toBe(3); // Todo
+  // });
+  // it('Should resolve conflicts', () => {
+  //     expect(1 + 1).toBe(3); // Todo
+  // });
+  // it('Should show proof of work', () => {
+  //     expect(1 + 1).toBe(3); // Todo
+  // });
+  // it('Should check for a valid chain', () => {
+  //     expect(1 + 1).toBe(3); // Todo
+  // });
+  // it('Should check the validity of a chain', () => {
+  //     expect(1 + 1).toBe(3); // Todo
+  // });
 });
-
